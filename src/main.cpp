4ee8fc549cfc7e2fb1630bb97e17d221977b6394
babbel056 <hermans.anthony@hotmail.com>
2017-05-14 19:45:41 +0200

    sending data between processes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include </usr/local/include/mpi.h>
 
 class remote{};
 class receiver{};
+
+class DummyData{
+  public:
+    DummyData(std::string i_msg, std::vector<int> i_vct): msg(i_msg), content(i_vct){}
+    std::string getMessage(){return msg;}
+    void printVector(){
+      std::cout << "Vectorcontents";
+      for (std::vector<int>::iterator it = content.begin(); it != content.end(); ++it)std::cout << ' ' << *it;
+      std::cout << std::endl;
+    }
+  private:
+    std::string msg;
+    std::vector<int> content;
+};
+
 int main(){
   std::cout << "MPI prototype\n";
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
-  // Based dummy on http://mpitutorial.com/tutorials/mpi-send-and-receive/
+
   // Find out rank, size
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  int number;
+  // Dummy code
+  DummyData data{"Hello world!", {1,2,3}};
   if (world_rank == 0) {
-      number = -1;
-      MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+      MPI_Send(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
   } else if (world_rank == 1) {
-      MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-      std::cout << "Process 1 received number " << number << " from process 0\n";
+      MPI_Recv(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      std::cout << "Process 1 received message(" << data.getMessage() << ") from process 0\n";
+      data.printVector();
   }
+
+  // Finalize the MPI environment.
   MPI_Finalize();
 }
