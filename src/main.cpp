57f5f9c004e866d880dda24bcca9fbbd6c800377
babbel056 <hermans.anthony@hotmail.com>
2017-05-18 15:06:31 +0200

    cleaner documentation, more info in host_file

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,37 @@
 #include "receiverDummy.h"
 #include "dataDummy.h"
 
+// Go to http://mpitutorial.com/tutorials/ for more information
+
 int main(){
   std::cout << "MPI prototype per process\n";
+
+  // Set up remote and receiver
+  std::vector<int> v = {1,2,3,4};
+  remoteDummy remote{0};
+  receiverDummy receiver{1};
+
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
-
-  std::vector<int> v = {1,2,3,4};
-  receiverDummy receiver{0};
-  remoteDummy remote{1};
   // Find out rank, size
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  // Dummy code
-  int tag = 10;
+  // Tag is used to specify the message ID
+  // When a process requests a message with a certain tag number,
+  // messages with different tags will be buffered by the network until the process is ready for them.
+  int tag = 1;
   dataDummy data = dataDummy("Hello world!", 5);
-  int occurrences = 0;
-  if (world_rank == 0) {
-      remote.sendData(&v, 1, remote.getId(), tag);
-      // remote.sendData(data, 1, remote.getId(), tag);
-      // tag++;
-      // times = rand();
-      // remote.sendData(array, 1, remote.getId(),tag);
+
+  if (world_rank == remote.getId()) {
+    // Remote will send vector v, 1 time, to the receiver with a specific tag
+    remote.sendData(&v, 1, receiver.getId(), tag);
   } else if (world_rank == 1) {
-      MPI_Status status;
-      receiver.receiveData(&v, 1, receiver.getId(), tag, status);
-      // receiver.receiveData(data, 1, receiver.getId(), tag, status);
-      std::cout << "Process 1 received message " << data.getMessage() << " from process 0\n";
-      // After receiving the message, check the status to determine
-      // how many numbers were actually received
-      MPI_Get_count(&status, MPI_INT, &occurrences);
-      std::cout << "Proc 1 received " << occurrences << " messages from " << status.MPI_SOURCE << " with tag "<< status.MPI_TAG << "\n";
+    // Receiver will receiva vector v, 1 time, from remote with a specific tag
+    receiver.receiveData(&v, 1, remote.getId(), tag);
+    std::cout << "Process 1 received message " << data.getMessage() << " from process 0\n";
   }
 
   // Finalize the MPI environment.
--- src/receiverDummy.h
+++ src/receiverDummy.h
@@ -6,8 +6,8 @@ class receiverDummy{
     receiverDummy(uint id): m_id(id){};
     ~receiverDummy() = default;
     template<class T>
-    void receiveData(T data, const int maxOccurrences,const int source, int tag, MPI_Status status){
-      MPI_Recv(&data, maxOccurrences, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+    void receiveData(T data, const int maxOccurrences,const int source, int tag){
+      MPI_Recv(&data, maxOccurrences, MPI_INT, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     uint getId() const {return m_id;}
   private:
